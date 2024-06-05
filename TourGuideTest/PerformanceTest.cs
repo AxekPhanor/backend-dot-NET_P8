e5543b3bc9f34036882f3b2d80125911646c29c6
Axek <axel.phanor64@gmail.com>
2024-06-05 13:28:38 +0200

    test: passage du nombre d'utilisateur de 100000 à 100 sur HighVolumeTrackLocation afin de réduire le temps d'execution du workflow

--- TourGuideTest/PerformanceTest.cs
+++ TourGuideTest/PerformanceTest.cs
@@ -48,7 +48,7 @@ namespace TourGuideTest
         public async Task HighVolumeTrackLocation()
         {
             //On peut ici augmenter le nombre d'utilisateurs pour tester les performances
-            _fixture.Initialize(100000);
+            _fixture.Initialize(100);
 
             List<User> allUsers = _fixture.TourGuideService.GetAllUsers();
 
